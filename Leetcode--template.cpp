ca159973c733cf069122152acf250752d344b28e
QianXi <qianxi.study@outlook.com>
2023-10-12 14:17:17 +0800

    改善模板

--- Leetcode--template.cpp
+++ Leetcode--template.cpp
@@ -5,23 +5,7 @@ using namespace std;
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
-        int indexLeft = 0,indexRight=nums.size();
-        while (indexLeft < indexRight){
-            int indexMid = indexLeft + (indexRight - indexLeft) / 2;
-            if (nums[indexMid] == target){
-                return indexMid;
-            }else if (nums[indexMid] < target){
-                indexLeft = indexMid + 1;
-            }else if (nums[indexMid] > target){
-                indexRight = indexMid ;
-            }
-        }
-        for (int i = 0; i <nums.size() ; ++i) {
-            if (nums[i] > target){
-                return i;
-            }
-        }
-        return int(nums.size());
+
     }
 };
 
